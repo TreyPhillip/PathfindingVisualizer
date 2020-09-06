dd19371a61b13a7357f66868bff1a724d6e7bf11
TreyPhillip <treyphillip@live.com>
2020-09-05 22:49:20 -0400

    Pathfinding partially working. Need to reopen cheaper nodes

--- PathfindingVisualizer.py
+++ PathfindingVisualizer.py
@@ -1,12 +1,15 @@
 import math
 import heapq
 import pygame
+import time
 
 # colours
 # standard node color and obstacle color
 WHITE = (255, 255, 255)
 GREY = (105,105,105)
 BLACK = (0,0,0)
+# open set
+ORANGE = (255,178,102)
 # start node
 GREEN = (0, 255, 0)
 # end node
@@ -31,7 +34,9 @@ class Node:
         self.width = width
         self.colour = WHITE
         self.parent = None
-        self.cost = 10
+        self.gcost = 10
+        self.hcost = 0
+        self.fcost = 0
 
     def getMyPosition(self):
         return self.row, self.col
@@ -40,7 +45,7 @@ class Node:
         pygame.draw.rect(display, self.colour, (self.x, self.y, self.width, self.width))
 
     def __lt__(self, other):
-        return False
+        return self.fcost < other.fcost
 
     def getNeighbors(self, grid):
         directions = [[1, 0], [0, 1], [-1, 0], [0, -1]]
@@ -48,14 +53,22 @@ class Node:
         for direction in directions:
             newRow = (self.row + direction[0])
             newCol = (self.col + direction[1])
-            neighbor = Node(newRow, newCol, MARGIN)
-            # neighbor = [self.row + direction[0], self.col + direction[1]]
-            print(neighbor)
-            if neighbor.row >= - 1 and neighbor.row >= -1:
-                print("ye its here")
+            if newRow > -1 and newCol > -1 and newRow < 40 and newCol < 40:
+                neighbor = grid[newRow][newCol]
                 result.append(neighbor)
+        # result = [item for item in result if item.col > -1 and item.row > -1]
         return result
 
+def getCostForNeighbors(neighbors):
+    for neighbor in neighbors:
+        getManhattanDistance(neighbor.row, neighbor.col)
+
+def getPosition():
+    position = pygame.mouse.get_pos()
+    column = position[0] // (WIDTH + MARGIN)
+    row = position[1] // (HEIGHT + MARGIN)
+    coords = (row, column)
+    return coords
 
 def draw():
     for row in range(40):
@@ -67,12 +80,14 @@ def draw():
                 colour = GREEN
             if grid[row][column].colour == BLUE:
                 colour = BLUE
-            pygame.draw.rect(display,
-                             colour,
-                             [(MARGIN + WIDTH) * column + MARGIN,
-                              (MARGIN + HEIGHT) * row + MARGIN,
-                              WIDTH,
-                              HEIGHT])
+            if grid[row][column].colour == ORANGE:
+                colour = ORANGE
+            if grid[row][column].colour == RED:
+                colour = RED
+            pygame.draw.rect(
+                display, colour, 
+                [(MARGIN + WIDTH) * column + MARGIN, 
+                 (MARGIN + HEIGHT) * row + MARGIN, WIDTH, HEIGHT])
 
 def getManhattanDistance(pt1, pt2):
     distance = 0
@@ -80,7 +95,6 @@ def getManhattanDistance(pt1, pt2):
         distance += abs(pt1[i] - pt2[i])
     return distance
 
- 
 # create grid
 grid = [] 
 for x in range(40):
@@ -96,7 +110,13 @@ done = False
 clock = pygame.time.Clock()
 
 # use Binary heap for open and closed nodes
+start = None
+current = None
+end = None
+# Create open set of nodes that are current canadites for examination
+# Begins with only the starting node inside
 open = []
+# Create closed set of nodes that have already been examined
 closed = []
 # heapq.heapify(open)
 # heapq.heappush(open, )
@@ -105,41 +125,49 @@ while not done:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             done = True
+        elif pygame.mouse.get_pressed()[0]:
+            row, col = getPosition()
+            grid[row][col].colour = GREY
+            print("Grid coordinates: ", row, col)
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s:    
-                position = pygame.mouse.get_pos()
-                column = position[0] // (WIDTH + MARGIN)
-                row = position[1] // (HEIGHT + MARGIN)
-                start = Node(row, column, MARGIN)
-                heapq.heappush(open, start)
-                print(open[0].colour)
-                grid[row][column].colour = GREEN
-            if event.key == pygame.K_e:
-                position = pygame.mouse.get_pos()
-                column = position[0] // (WIDTH + MARGIN)
-                row = position[1] // (HEIGHT + MARGIN)
-                grid[row][column].colour = BLUE
-            if event.key == pygame.K_SPACE:
+                if not start:
+                    row, col = getPosition()
+                    start = grid[row][col]
+                    grid[row][col].colour = GREEN
+                    heapq.heappush(open, start)
+            elif event.key == pygame.K_e:
+                if not end:
+                    row, col = getPosition()
+                    end = grid[row][col]
+                    grid[row][col].colour = BLUE           
+            elif event.key == pygame.K_SPACE:
                 if not open:
-                        print("select a start node")
-                elif open[0].colour != BLUE:                  
-                    print("not at end node. Searching...")
-                    current = heapq.heappop(open)
-                    heapq.heappush(closed, current)
-                    neighbors = current.getNeighbors(grid)
-                    # for neighbor in neighbors:
-                    #     print(neighbor)
-        
-        elif pygame.mouse.get_pressed()[0]:
-            position = pygame.mouse.get_pos()
-            column = position[0] // (WIDTH + MARGIN)
-            row = position[1] // (HEIGHT + MARGIN)
-            grid[row][column].colour = GREY
-            print("Click ", position, "Grid coordinates: ", row, column)
-
-
+                    print("select a start node")
+                elif not end:
+                    print("select an end node")
+                else:
+                    while current != end:   
+                        endLocation = end.getMyPosition()           
+                        print("not at end node. Searching...")
+                        # get the best node from open set 
+                        # and make that current node
+                        current = heapq.heappop(open)
+                        if current.colour != BLUE and current.colour != GREEN:
+                            current.colour = RED                      
+                        heapq.heappush(closed, current)
+                        # get neighbors for current node
+                        neighbors = current.getNeighbors(grid)
+                        # get F cost for each neighbor
+                        for neighbor in neighbors:
+                            neighbor.gcost += current.gcost
+                            neighbor.hcost = getManhattanDistance(neighbor.getMyPosition(), endLocation)
+                            neighbor.fcost = neighbor.gcost + neighbor.hcost
+                            if neighbor.colour != BLUE and neighbor.colour != GREEN:
+                                neighbor.colour = ORANGE
+                            heapq.heappush(open, neighbor)
+                    print("FOUND YOU")
     display.fill(BLACK)
-
     # draw
     draw()
     clock.tick(60)
@@ -149,9 +177,8 @@ while not done:
 
 # Nodes have F value with is the addition of the G value and H value
 # G value
-# Create open set of nodes that are current canadites for examination
-# Begins with only the starting node inside
-# Create closed set of nodes that have already been examined
+
+
 # each node keeps a pointer to its parent node
 #
 # get manhattan distance for nodes to end node
